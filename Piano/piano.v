0c3df19fed1fb86f9e402fc7fc0b588deaa5d750
fumi <fumi01200924@gmail.com>
2020-03-12 15:24:48 +0900

    加算するの忘れてた

--- Piano/piano.v
+++ Piano/piano.v
@@ -24,39 +24,58 @@ always @(posedge CLK_IN) begin
 		if(counter[0]==12000000/523) begin
 			wave[0] <= ~wave[0];
 			counter[0] <= 0;
-			end;
+		end else begin
+			counter[0] <= counter[0]+24'd1;
+		end
+		
 		if(counter[1]==12000000/587) begin
 			wave[1] <= ~wave[1];
 			counter[1] <= 0;
-			end;
+		end else begin
+			counter[1] <= counter[1]+24'd1;
+		end
+		
 		if(counter[2]==12000000/659) begin
 			wave[2] <= ~wave[2];
 			counter[2] <= 0;
-			end;
+		end else begin
+			counter[2] <= counter[2]+24'd1;
+		end
+		
 		if(counter[3]==12000000/698) begin
 			wave[3] <= ~wave[3];
 			counter[3] <= 0;
-			end;
+		end else begin
+			counter[3] <= counter[3]+24'd1;
+		end
+		
 		if(counter[4]==12000000/783) begin
 			wave[4] <= ~wave[4];
 			counter[4] <= 0;
-			end;
-		if(counter[4]==12000000/880) begin
-			wave[4] <= ~wave[4];
-			counter[4] <= 0;
-			end;
+		end else begin
+			counter[4] <= counter[4]+24'd1;
+		end
+		
 		if(counter[5]==12000000/987) begin
 			wave[5] <= ~wave[5];
 			counter[5] <= 0;
-			end;
+		end else begin
+			counter[5] <= counter[5]+24'd1;
+		end
+		
 		if(counter[6]==12000000/1046) begin
 			wave[6] <= ~wave[6];
 			counter[6] <= 0;
-			end;
+		end else begin
+			counter[6] <= counter[6]+24'd1;
+		end
+		
 		if(counter[7]==12000000/2274) begin
 			wave[7] <= ~wave[7];
 			counter[7] <= 0;
-			end;
+		end else begin
+			counter[7] <= counter[7]+24'd1;
+		end
 	end
 end
 assign BZ = (!SW[0]&&wave[0])^
